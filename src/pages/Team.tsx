ac592aa78e2d2a0345d5bd702473f1ba1f77f1bf
mohammedaasim-dev <smohammedaasim5@gmail.com>
2025-12-28 15:46:17 +0000

    Add Prakruthi V image and reference in Team.tsx

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -78,7 +78,7 @@ const domainTeams = [
   {
     name: "Hospitality",
     roles: [
-      { title: "Head", members: [{ name: "A. Jagadeesh", image: "/images/team/a-jagadeesh.jpg" }, { name: "Prakruthi V" }] },
+      { title: "Head", members: [{ name: "A. Jagadeesh", image: "/images/team/a-jagadeesh.jpg" }, { name: "Prakruthi V", image: "/images/team/prakruthi-v.jpg" }] },
       { title: "Lead", members: [{ name: "Prithwi Ganesh Goud" }] },
       { title: "Co-Lead", members: [{ name: "Sri Nandini T" }, { name: "Vakul Kumar Bayya" }] },
     ]
