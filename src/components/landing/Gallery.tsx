7e83a63d81685f760ece68d5260f5d81cec0699f
siddu1236789-lang <siddu1236789@gmail.com>
2025-12-31 11:59:16 +0000

    gallery: keep existing remote images and append local uploaded photos

--- src/components/landing/Gallery.tsx
+++ src/components/landing/Gallery.tsx
@@ -2,22 +2,33 @@ import { motion } from "framer-motion";
 
 const images = [
   // Row 1: Large 2x2 block + two 1x1 blocks
-  { src: "/photos/IMG_9091.jpg", span: "md:col-span-2 md:row-span-2" },
-  { src: "/photos/IMG_8609.jpg", span: "md:col-span-1 md:row-span-1" },
-  { src: "/photos/IMG_8627.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/9b83c360-d2d8-4072-af5d-de072d60ea45", span: "md:col-span-2 md:row-span-2" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/12458a42-1ff8-46cd-82ce-ce4f8ee6646a", span: "md:col-span-1 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/b22d21d2-de38-4404-ad2d-a2e803923c87", span: "md:col-span-1 md:row-span-1" },
   
   // Row 2: (2x2 continues) + one 2x1 block
-  { src: "/photos/DSC02186.jpg", span: "md:col-span-2 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/fd275c1e-e4c8-4b1b-99ff-f7dc59e42812", span: "md:col-span-2 md:row-span-1" },
   
   // Row 3: One 1x2 block + one 2x1 block + one 1x1 block
-  { src: "/photos/DSC02472.jpg", span: "md:col-span-1 md:row-span-2" },
-  { src: "/photos/DSC_6645.jpg", span: "md:col-span-2 md:row-span-1" },
-  { src: "/photos/IMG_7835.JPG", span: "md:col-span-1 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/a7281552-3704-4875-9540-df5c0d74010c", span: "md:col-span-1 md:row-span-2" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/368d82b1-f481-4a85-afe0-8e2f405c6ba4", span: "md:col-span-2 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/97be7b71-737a-479c-ad45-8487e55aac2a", span: "md:col-span-1 md:row-span-1" },
   
   // Row 4: (1x2 continues) + three 1x1 blocks
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/4a4483fb-f891-4ba3-ad86-a4101be4ab65", span: "md:col-span-1 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/26d2793b-cc75-4b2c-8295-a2dd33fc0482", span: "md:col-span-1 md:row-span-1" },
+  { src: "https://harmless-tapir-303.convex.cloud/api/storage/be4df044-590b-4e98-888c-bd3070f6ab28", span: "md:col-span-1 md:row-span-1" },
+
+  // Appended: local uploaded photos (RAW omitted)
+  { src: "/photos/IMG_9091.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "/photos/IMG_8609.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "/photos/IMG_8627.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "/photos/DSC02186.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "/photos/DSC02472.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "/photos/DSC_6645.jpg", span: "md:col-span-1 md:row-span-1" },
+  { src: "/photos/IMG_7835.JPG", span: "md:col-span-1 md:row-span-1" },
   { src: "/photos/IMG_7847.JPG", span: "md:col-span-1 md:row-span-1" },
   { src: "/photos/IMG_9283.jpg", span: "md:col-span-1 md:row-span-1" },
-  { src: "/photos/IMG_9091.jpg", span: "md:col-span-1 md:row-span-1" },
 ];
 
 export default function Gallery() {
